c28e4cad9d426465f4320256c657c5b9b31d8d7f
Denis Lochman <denis.lochman@protonmail.com>
2023-09-11 12:01:48 +0200

    refactor/fix tests

--- src/requests/requests_reader_test.go
+++ src/requests/requests_reader_test.go
@@ -5,16 +5,18 @@ import (
 	"github.com/fluentassert/verify"
 )
 
+const testPath = "../../test/test_files"
+
 func TestReadRequestsInfo_EmptyFolder_ReturnsEmpty(t *testing.T) {
-    result := ReadRequestsInfo("../test/test_files/empty_folder")
+    result := ReadRequestsInfo(testPath + "/empty_folder")
 
     verify.Slice[RequestInfo](result).Empty().Assert(t)
 }
 
 func TestReadRequestsInfo_NonEmptyFolder_ReturnsHurlFiles(t *testing.T) {
-    result := ReadRequestsInfo("../test/test_files/")
+    result := ReadRequestsInfo(testPath)
 
     verify.Slice[RequestInfo](result).Should(func(got []RequestInfo) bool { return len(got) == 2 }).Assert(t)
-    verify.String(result[0].name).Equal("test_1.hurl")
-    verify.String(result[1].name).Equal("test_2.hurl")
+    verify.String(result[0].name).Equal("test_1.hurl").Assert(t)
+    verify.String(result[1].name).Equal("test_2.hurl").Assert(t)
 }
